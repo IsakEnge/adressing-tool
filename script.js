7567528d32f2ab644055334eabf302946ecadb47
IsakEnge <82433688+IsakEnge@users.noreply.github.com>
2024-01-16 15:23:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function addLayer() {
+    console.log('addLayer function called');
     // Legg til logikk for å legge til layers
 }
 
 function showLightInfo(address, channel) {
+    console.log('showLightInfo function called');
     // Viser lysinformasjon og fyller inn feltene
     document.getElementById('address').value = address;
     document.getElementById('channel').value = channel;
@@ -10,6 +12,7 @@ function showLightInfo(address, channel) {
 }
 
 function saveChanges() {
+    console.log('saveChanges function called');
     // Lagre endringer logikk
     // Du kan hente verdier fra input-feltene ved å bruke document.getElementById('address').value, etc.
 }
